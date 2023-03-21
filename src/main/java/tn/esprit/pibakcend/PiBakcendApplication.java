5275c41d7539a05cd8d0f076a7b21a9cf3fbc44d
ibrahim ben abderrahman <you@example.com>
2023-03-21 16:53:56 +0100

    entities

---
+++ src/main/java/tn/esprit/pibakcend/PiBakcendApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.pibakcend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiBakcendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiBakcendApplication.class, args);
+    }
+
+}
