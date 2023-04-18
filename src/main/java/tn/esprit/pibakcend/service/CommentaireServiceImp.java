c49bbf68d18099a05b24c5ca9aadb9d663ddc382
Chebbi_raed <61346201+ROPOROS@users.noreply.github.com>
2023-04-18 21:42:11 +0100

    almost there + SMS

--- src/main/java/tn/esprit/pibakcend/service/CommentaireServiceImp.java
+++ src/main/java/tn/esprit/pibakcend/service/CommentaireServiceImp.java
@@ -1,6 +1,7 @@
 package tn.esprit.pibakcend.service;
 
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tn.esprit.pibakcend.entities.Commentaire;
 import tn.esprit.pibakcend.entities.Publication;
@@ -21,6 +22,8 @@ public class CommentaireServiceImp implements ICommentaire{
 
     PublicationRepository publicationRepository;
 
+    private TwillioSmsSender twillioSmsSender;
+
     @Override
     public Commentaire updateComm(Commentaire comm,Integer idComm) {
         Commentaire commentaire = commentaireRepository.findById(idComm).orElse(null);
@@ -35,6 +38,7 @@ public class CommentaireServiceImp implements ICommentaire{
         return commentaireRepository.findAll().stream().filter(x -> x.getPublication().getIdPub() == idPub).collect(Collectors.toList());
     }
 
+
     @Override
     public Commentaire assignCommentaireToPub(Commentaire comm, Integer idPub, Long idUser) {
         Publication publication = publicationRepository.findById(idPub).orElse(null);
@@ -43,6 +47,13 @@ public class CommentaireServiceImp implements ICommentaire{
             comm.setPublication(publication);
             comm.setUser(user);
             commentaireRepository.save(comm);
+            // Send SMS notification to the user who created the publication
+            User pubUser = publication.getUser();
+            if (pubUser != null) {
+                String message = String.format("%s commented on your publication '%s'", user.getUsername(), publication.getTitrePub());
+                SmsRequest smsRequest = new SmsRequest(pubUser.getTelNumber(), message);
+                twillioSmsSender.sendSms(smsRequest);
+            }
             return comm;
         }
         return null;
---
+++ src/main/java/tn/esprit/pibakcend/service/SmsSender.java
@@ -0,0 +1,6 @@
+package tn.esprit.pibakcend.service;
+
+public interface SmsSender {
+    void sendSms(SmsRequest smsRequest);
+
+}
