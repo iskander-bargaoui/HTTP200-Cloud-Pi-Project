5e04c1b1d80dd598503a191dbe6b27454a68107d
nourhene chalghoumi <115397025+nourhenechalghoumi@users.noreply.github.com>
2023-04-18 01:49:18 +0100

    Commit Nour 1

---
+++ src/main/java/com/webtutsplus/ecommerce/Entity/Notification.java
@@ -0,0 +1,16 @@
+package com.webtutsplus.ecommerce.Entity;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+public class Notification implements Serializable {
+    @Id
+    public String message ;
+    @Temporal(TemporalType.DATE)
+    public  Date dateNotification ;
+    public int idSource ;
+}
---
+++ src/main/java/com/webtutsplus/ecommerce/enums/OrderStatus.java
@@ -0,0 +1,12 @@
+package com.webtutsplus.ecommerce.enums;
+
+
+public enum OrderStatus {
+    PENDING,
+    ACCEPTED,
+    REJECTED,
+    PAID,
+    DELIVERED
+
+}
+
