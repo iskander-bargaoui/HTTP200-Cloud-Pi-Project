5d1e43a69e7406cee8866c565d8fd72812246248
Griguich <ghazi.griguich@esprit.tn>
2023-04-22 01:27:49 +0100

    comm1

---
+++ src/main/java/tn/esprit/pibakcend/entities/Reservation.java
@@ -0,0 +1,47 @@
+package tn.esprit.pibakcend.entities;
+
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.security.Timestamp;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+//// Constructor par defaults
+@NoArgsConstructor
+@ToString
+public class Reservation implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long idReservation ;
+    @Temporal(TemporalType.DATE)
+    public Date dateReservetion;
+
+//ksdhgcxvb lcsjhdxc kjsl,d
+    @ManyToOne
+    public User UserR ;
+
+    @ManyToOne
+    public Notification notification;
+}
