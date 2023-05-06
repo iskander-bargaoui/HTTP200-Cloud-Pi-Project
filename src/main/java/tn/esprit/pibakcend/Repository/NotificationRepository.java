dd1205dbf09ec12546e7fe73d60069bfb10cfa78
nourhene chalghoumi <115397025+nourhenechalghoumi@users.noreply.github.com>
2023-05-07 00:31:25 +0100

    Delete src/main directory

--- src/main/java/tn/esprit/pibakcend/Repository/NotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package tn.esprit.pibakcend.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.esprit.pibakcend.entities.Notification;
-
-public interface NotificationRepository extends JpaRepository<Notification,Long> {
-}
